390486c8a48c294959548ddf98c5b7862ebfe278
N-DiLo <egbekwunwanedilobu@gmail.com>
2023-08-11 02:09:47 +0100

    feat: updated kotlin version and google_fonts

--- lib/utils/constant/app_color.dart
+++ lib/utils/constant/app_color.dart
@@ -12,3 +12,19 @@ class AppPalette {
   static const lnColor = Color(0xff0A66C2);
   static const fbColor = Color(0xff1F4D9B);
 }
+
+//App Colors
+const kcFBColor = Color(0xff1F4D9B);
+const kcLNColor = Color(0xff0A66C2);
+const kcTWColor = Color(0xff54C5F8);
+const primaryColor = Color(0xffD42A9A);
+const defaultColor = Color(0xff000000);
+const whiteColor = Color(0xffFFFFFF);
+const textColor = Color(0xff1E1E1E);
+const textColor1 = Color(0xff2F2E41);
+const fontColor = Color(0xff242222);
+const buttonColor = Color(0xffF7F8FA);
+
+//App Images
+const appLogo = 'assets/app-logo.png';
+const profileImage = 'assets/profile.jpg';
